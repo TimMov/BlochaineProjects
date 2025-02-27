4c9dff871b90a9d4f7b47eed7e8946fcb59faf7c
TimMov <tgasimov6@mail.ru>
2025-02-27 23:12:43 +0300

    /

--- contracts/ItContract.sol
+++ contracts/ItContract.sol
@@ -8,4 +8,23 @@ contract ItContract {
     uint256 public totalSupply;
 
     mapping (address => uint256) public balance0f;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    constructor(uint256 _initialSupply){
+
+        totalSupply = _initialSupply * 10 ** uint256(decimals);
+        balance0f[msg.sender] = totalSupply;
+
+    }
+
+    function transferToken(address _to, uint256 _value) public returns (bool success){
+
+        require(balance0f[msg.sender] >= _value);  
+        balance0f[msg.sender] -= _value;  
+        balance0f[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+
+    }
 }

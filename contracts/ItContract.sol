b3c6736878c4e473ab2a1367bcd9bd5ab0d07592
TimMov <tgasimov6@mail.ru>
2025-02-27 22:29:40 +0300

    обучение пошло

---
+++ contracts/ItContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract ItContract {
+    string public name = "TimMov";
+    string public symbol = "TM";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping (address => uint256) public balance0f;
+}

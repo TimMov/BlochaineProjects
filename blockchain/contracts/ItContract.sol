ada88b68295d036d98bd62f49c8abf95945dd928
TimMov <tgasimov6@mail.ru>
2025-04-10 20:29:57 +0300

    изменение

--- blockchain/contracts/ItContract.sol
+++ blockchain/contracts/ItContract.sol
@@ -7,11 +7,11 @@ contract DiplomaRegistry {
         string universityName;
         uint256 year;
     }
-    
+
     Diploma[] private diplomas;
-    
+
     event DiplomaAdded(uint256 indexed id, string studentName, string universityName, uint256 year);
-    
+
     function addDiploma(
         string calldata _studentName,
         string calldata _universityName,
@@ -20,22 +20,22 @@ contract DiplomaRegistry {
         require(bytes(_studentName).length > 0, "Student name cannot be empty");
         require(bytes(_universityName).length > 0, "University name cannot be empty");
         require(_year >= 1900 && _year <= 2100, "Invalid graduation year");
-        
+
         diplomas.push(Diploma(_studentName, _universityName, _year));
         emit DiplomaAdded(diplomas.length - 1, _studentName, _universityName, _year);
     }
-    
+
     function getDiplomasCount() external view returns (uint256) {
         return diplomas.length;
     }
-    
-    function getDiploma(uint256 _index) external view returns (
-        string memory studentName,
-        string memory universityName,
-        uint256 year
-    ) {
-        require(_index < diplomas.length, "Index out of bounds");
-        Diploma memory diploma = diplomas[_index];
-        return (diploma.studentName, diploma.universityName, diploma.year);
+
+    function getDiploma(uint256 index) external view returns (string memory, string memory, uint256) {
+        require(index < diplomas.length, "Diploma index out of range");
+        Diploma storage d = diplomas[index];
+        return (d.studentName, d.universityName, d.year);
+    }
+
+    function getAllDiplomas() external view returns (Diploma[] memory) {
+        return diplomas;
     }
 }
